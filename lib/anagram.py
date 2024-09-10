ee70fa035314974bfaaccf95d16a0fd488449889
mark <kipkoechtony510@gmail.com>
2024-09-10 23:32:23 +0300

    Add Anagram class to detect anagrams

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,16 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, possible_anagrams):
+        # Get sorted version of the original word
+        sorted_word = ''.join(sorted(self.word))
+        
+        # Filter possible anagrams
+        result = []
+        for candidate in possible_anagrams:
+            if ''.join(sorted(candidate)) == sorted_word and candidate != self.word:
+                result.append(candidate)
+                
+        return result
